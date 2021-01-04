14812db6ddbc9bde8eb1ff46bd3f58988d9abb8d
PrzemekCraker <przemekzbigniew@gmail.com>
2021-01-04 14:41:57 +0100

    prettier-change#2

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const ColorCounter = ({ color, onIncrease, onDecrease }) => {
-    return (
+  return (
     <View>
-        <Text>{color}</Text>
-        <Button onPress={() => onIncrease()} title={`Increase ${color}`}/>
-        <Button onPress={() => onDecrease()} title={`Decrease ${color}`}/>
+      <Text>{color}</Text>
+      <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
+      <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
     </View>
-    );
+  );
 };
 
 const styles = StyleSheet.create({});
--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = ({ imageSource, title, score }) => {
-    return <View>
-        <Image source={imageSource}/>
-        <Text>{title}</Text>
-        <Text>Image score - {score}</Text>
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image score - {score}</Text>
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({});
 
